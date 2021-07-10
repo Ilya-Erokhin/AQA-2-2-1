5a93d1523d6a74ba4d0ecf80610b4f49eedb14ea
Ilya Erokhin <illyaerohin@yandex.ru>
2021-07-10 21:21:48 +0300

    init

---
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -0,0 +1,66 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CardOrderTest {
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+    @Test
+    void shouldTest() {
+        $("[data-test-id=name] input").setValue("Илья Ерохин");
+        $("[data-test-id=phone] input").setValue("+79113049711");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldTestForm() {
+        $("[data-test-id=name] input").setValue("Илья Ерохин-Кравц");
+        $("[data-test-id=phone] input").setValue("+79370000000");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTestFieldName() {
+        $("[data-test-id=name] input").setValue("Ilya Erpkhin");
+        $("[data-test-id=phone] input").setValue("+79370000000");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldTestFieldPhone() {
+        $("[data-test-id=name] input").setValue("Илья Ерохин");
+        $("[data-test-id=phone] input").setValue("89370000000");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldTestFieldAgreement() {
+        $("[data-test-id=name] input").setValue("Илья Ерохин");
+        $("[data-test-id=phone] input").setValue("89370000000");
+        $("[data-test-id=agreement] .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $("[type='button']").click();
+    }
+
+    @Test
+    void shouldTestFieldNonInput() {
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+}
